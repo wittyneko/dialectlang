4b8e95c53939b50d380317cbb1eaaa20dc916602
wittyneko <wittytutu@gmail.com>
2020-02-18 17:49:15 +0800

    修改存在歧义的语法

--- dialect/src/main/java/com/wittyneko/dialect/zh/控制台.kt
+++ dialect/src/main/java/com/wittyneko/dialect/zh/控制台.kt
@@ -9,7 +9,7 @@ inline fun Any?.打印() = print(this)
 inline fun Any?.打印换行() = println(this)
 inline fun Any?.打印(前缀: 字符串 = "", 后缀: 字符串 = "") = print("$前缀$this$后缀")
 inline fun Any?.打印换行(前缀: 字符串 = "", 后缀: 字符串 = "") = println("$前缀$this$后缀")
-inline fun 打印换行() = println()
+inline fun 换行打印() = println()
 
 inline fun 读取一行() = readLine()
 
